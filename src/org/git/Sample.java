4ca0ce74f37ce4c85d59634087f5ed7086c9d513
gokul18nath <gokulnathjayaraman18@gmail.com>
2022-08-30 13:55:13 +0530

    Program is added

---
+++ src/org/git/Sample.java
@@ -0,0 +1,15 @@
+package org.git;
+
+public class Sample {
+	
+	private void hello() {
+		System.out.println("Hello world");
+
+	}
+	public static void main(String[] args) {
+		 
+		Sample s= new Sample();
+		s.hello();
+	}
+
+}
